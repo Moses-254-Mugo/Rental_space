f3fed5a385215375f7f042f0ffdfb9417aaa8353
Marvin Maina <machariamarvin625@gmail.com>
2021-06-23 00:19:19 +0300

    Add popup login and signup forms

---
+++ assets/rentals.js
@@ -0,0 +1,14 @@
+function login(){
+    document.getElementById("signIN").style.display = "block"
+    document.getElementById("signUP").style.display = "none"
+}
+
+function signup(){
+    document.getElementById("signUP").style.display = "block"
+    document.getElementById("signIN").style.display = "none"
+}
+
+function hideForm(){
+    document.getElementById("signIN").style.display = "none"
+    document.getElementById("signUP").style.display = "none"
+}
