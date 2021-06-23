f7f358f24bb533f36bd0853e2a9a7d6dc50753a7
Marvin Maina <machariamarvin625@gmail.com>
2021-06-23 15:42:50 +0300

    Validate signup form

--- assets/rentals.js
+++ assets/rentals.js
@@ -13,31 +13,80 @@ function hideForm(){
     document.getElementById("signUP").style.display = "none"
 }
 
-function validate1(){
-    var x = document.getElementsByClassName("input-user").value;
-    var y = document.getElementsByClassName("input-pwd").value;
-    var z = document.getElementById("input-pwd2").value;
-    if(x === "");
-    if(y === "");
-    if(z === ""){
-        alert("Please enter your details");
-        alert("please enter a valid password");
-        alert("please confirm your password");
+
+function validate(){
+    var a = document.getElementById("input-user").value;
+    var b = document.getElementById("input-pwd").value;
+    var c = document.getElementById("validate-user");
+    var d = document.getElementById("validate-pwd");
+    if(a === null || a === "" && b != ""){
+       c.style.display = "block";
+       d.style.display = "none";
+       return false;
     }
+
+    else if(a === null || a === "" ){
+    c.style.display = "block";
+    d.style.display = "none"
+    return false;
+    }
+
+    else if (b === null || b === ""){
+        d.style.display = "block";
+        c.style.display = "none"
+        return false
+    }
+    
     else{
-        alert("Account created successfully")
+        alert("Login successful");
+        c.style.display = "none";
+        d.style.display = "none";
+        return true;
     }
 }
 
-function validate(){
-    var x = document.getElementById("input-user").value;
-    var y = document.getElementById("input-pwd").value;
-    if(x === "");
-    if(y === ""){
-        alert("Please enter your details");
-        alert("please enter a valid password");
+function validate1(){
+    var a = document.getElementById("input-user").value;
+    var b = document.getElementById("input-pwd").value;
+    var c = document.getElementById("input-pwd2").value;
+    var d = document.getElementById("validate-user1");
+    var e = document.getElementById("validate-pwd1");
+    var f = document.getElementById("validate-pwd2");
+    var s = document.getElementById("validate-pwd3");
+    if(a === null || a === "" && b != ""){
+       d.style.display = "block";
+       e.style.display = "none";
+       return false;
     }
+
+    else if(a === null || a === "" ){
+    d.style.display = "block";
+    e.style.display = "none"
+    return false;
+    }
+
+    else if (b === null || b === ""){
+        e.style.display = "block";
+        d.style.display = "none"
+        return false
+    }
+    else if(c === null || c === ""){
+        f.style.display = "block";
+        e.style.display = "none";
+        s.style.display = "none"
+    }
+    else if (b != c){
+        s.style.display = "block";
+        f.style.display = "none";
+        e.style.display = "none";
+        return false;
+    }    
     else{
-        alert("Login successful")
+        alert("Account has been created");
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+        s.style.display = "none";
+        return true;
     }
 }
