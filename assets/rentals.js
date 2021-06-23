c7b6247940042e8f1bd3c3a9f3c79c8310c49098
Marvin Maina <machariamarvin625@gmail.com>
2021-06-23 10:25:08 +0300

    Validate login and signup form

--- assets/rentals.js
+++ assets/rentals.js
@@ -12,3 +12,32 @@ function hideForm(){
     document.getElementById("signIN").style.display = "none"
     document.getElementById("signUP").style.display = "none"
 }
+
+function validate1(){
+    var x = document.getElementsByClassName("input-user").value;
+    var y = document.getElementsByClassName("input-pwd").value;
+    var z = document.getElementById("input-pwd2").value;
+    if(x === "");
+    if(y === "");
+    if(z === ""){
+        alert("Please enter your details");
+        alert("please enter a valid password");
+        alert("please confirm your password");
+    }
+    else{
+        alert("Account created successfully")
+    }
+}
+
+function validate(){
+    var x = document.getElementById("input-user").value;
+    var y = document.getElementById("input-pwd").value;
+    if(x === "");
+    if(y === ""){
+        alert("Please enter your details");
+        alert("please enter a valid password");
+    }
+    else{
+        alert("Login successful")
+    }
+}
